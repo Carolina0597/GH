427d4ffcbc10bec0e5b385075454ce9b7679a376
carolina <caritho.restrepo.05@gmail.com>
2026-06-05 20:45:13 +0000

    reset

--- separator.tsx
+++
@@ -1,6 +0,0 @@
-"use client"
-import { cn } from "@/lib/utils"
-function Separator({ orientation='horizontal', className, ...props }: { orientation?: 'horizontal'|'vertical'; className?: string; [k:string]:any }) {
-  return <div className={cn("bg-border", orientation==='horizontal'?"h-px w-full":"h-full w-px", className)} {...props}/>
-}
-export { Separator }
